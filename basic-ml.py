7e679cfba6b7e1cb332b0864f1103576cfa18192
gmarinho2 <104592062+gmarinho2@users.noreply.github.com>
2024-05-18 22:31:32 -0300

    Adiciona função perda

--- basic-ml.py
+++ basic-ml.py
@@ -1,4 +1,8 @@
 import numpy as np
 
-def preve(X, w, b):
+def prever(X, w, b):
     return X * w + b
+
+def perda(X, w, b, lr): #lr é learning rat, o quanto o peso aumenta ou diminui
+    perda_total = np.average((preve(X, w, b) - y) ** 2)
+    return perda_total
